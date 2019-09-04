19b1437580522d59009f1c762cc1211d0f903df8
gautamz07 <gautamz07@gmail.com>
2019-09-04 14:08:31 +0530

    Initial commit
    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(function () {
+	
+	$(".skill-circle").circliful({
+	       animationStep: 5,
+	       foregroundBorderWidth: 15,
+	       backgroundBorderWidth: 15,
+	       backgroundColor: '#eee',
+	       // percent: 100,
+	       // replacePercentageByText: true,
+	       // text: 'lala',
+	       // icon: 'f13c',
+	       textBelow: true,
+	       animation: 0,
+	       percent: false,
+	       iconPosition: 'middle',
+	       percentageTextSize: 15,
+	}, function(){
+
+	});
+
+});
