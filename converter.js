fb89516a71ac771450edfce93acdeb4b51839ece
gautamgoshopmatic <gautam.fonseca@goshopmatic.com>
2023-03-21 17:08:27 +0530

    some more changes

--- converter.js
+++ converter.js
@@ -1,6 +1,6 @@
 var html_to_pdf = require('html-pdf-node');
 
-let options = { format: 'A4' };
+let options = { format: 'A4'  };
 // Example of options with args //
 // let options = { format: 'A4', args: ['--no-sandbox', '--disable-setuid-sandbox'] };
 
@@ -10,7 +10,9 @@ let file = { content: "<h1>Welcome to html-pdf-node</h1>" };
 let file = { url: "https://example.com" };
 */
 
-let file = { url: "./index.html" };
+// let file = { url: "https://gautamz07.github.io/gautamz05/index.html" };
+let file = { url: "https://gautamz07.github.io/gautamz05/index.html", name: "example.pdf" };
+// let file = { content: "index.html" };
 html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
   console.log("PDF Buffer:-", pdfBuffer);
 });
